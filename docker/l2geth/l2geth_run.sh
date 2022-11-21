9a7ace325705eb0e0dac4cb4058199aebc541dd1
HAOYUatHZ <haoyu@protonmail.com>
2022-11-21 10:41:49 +0800

    fix

--- docker/l2geth/l2geth_run.sh
+++ docker/l2geth/l2geth_run.sh
@@ -15,4 +15,4 @@ exec geth --mine --datadir "." --unlock 0 --password "./password" --allow-insecu
   --http --http.addr "0.0.0.0" --http.port 8545 --http.api "eth,scroll,net,web3,debug" \
   --ws --ws.addr "0.0.0.0" --ws.port 8546 --ws.api "eth,scroll,net,web3,debug" \
   --gcmode archive \
-  --trace.mptwitness
+  --trace.mptwitness 2
