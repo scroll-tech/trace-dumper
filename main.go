55a9cb3bbff67a3941476b383c120d58f8c910b9
Jerry Ho <a29988122@gmail.com>
2023-05-26 19:06:53 +0800

    add native in main.go and readme

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 	endpoint = flag.String("endpoint", "ws://127.0.0.1:8546", "The endpoint to connect to blockchain node")
 	keystore = flag.String("keystore", "./docker/l2geth/genesis-keystore", "Keystore file path")
 	password = flag.String("password", "scrolltest", "The keystore password")
-	dump     = flag.String("dump", "erc20", "e.g: erc20, nft, greeter, sushi, dao, uniswapv2")
+	dump     = flag.String("dump", "erc20", "e.g: erc20, native, nft, greeter, sushi, dao, uniswapv2")
 )
 
 func init() {
