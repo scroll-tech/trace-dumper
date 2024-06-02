225ac0938a62783c89a4ecc864e6b12a1b298916
Zhuo Zhang <mycinbrin@gmail.com>
2024-06-02 07:49:26 +0000

    fix

--- docker/l2geth/l2geth_run.sh
+++ docker/l2geth/l2geth_run.sh
@@ -14,4 +14,5 @@ fi
 exec geth --mine --datadir "." --unlock 0 --password "./password" --allow-insecure-unlock --verbosity 3 \
   --http --http.addr "0.0.0.0" --http.port 8545 --http.api "eth,scroll,net,web3,debug" \
   --ws --ws.addr "0.0.0.0" --ws.port 8546 --ws.api "eth,scroll,net,web3,debug" \
+  --miner.etherbase 1c5a77d9fa7ef466951b2f01f724bca3a5820b63 \
   --gcmode archive
